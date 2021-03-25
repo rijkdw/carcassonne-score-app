29b5bdd3e6357c721c97362f5f10d7b43ffa5fa6
Rijk <rijkdw@gmail.com>
2021-03-25 15:28:39 +0200

    Moved the NewGameForm into the NewGameScreen; had to use keys to achieve this

--- lib/screens/new_game_screen.dart
+++ lib/screens/new_game_screen.dart
@@ -1,17 +1,168 @@
-import 'package:carcassonne_score_app/widgets/forms/new_game_form.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:carcassonne_score_app/managers/games_manager.dart';
+import 'package:carcassonne_score_app/objects/game.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
-class NewGameScreen extends StatelessWidget {
+import '../utils/colour_utils.dart' as colour_utils;
+import '../utils/bool_utils.dart' as bool_utils;
+import '../utils/list_utils.dart' as list_utils;
+
+class NewGameScreen extends StatefulWidget {
+  @override
+  _NewGameScreenState createState() => _NewGameScreenState();
+}
+
+class _NewGameScreenState extends State<NewGameScreen> {
+  var formKey = GlobalKey();
+  var gameNameTextEditingController = TextEditingController();
+  var playerNameTextEditingControllers = List.generate(6, (index) => TextEditingController());
+
+  var colours = ['Red', 'Blue', 'Yellow', 'Green', 'Gray', 'Black'];
+
+  final GlobalKey<ScaffoldState> scaffoldkey = new GlobalKey<ScaffoldState>();
+
+  void trimTextEditingControllers() {
+    playerNameTextEditingControllers.forEach((c) => c.text = c.text.trim());
+  }
+
+  bool hasEnoughPlayers() {
+    return list_utils.count(playerNameTextEditingControllers.map((c) => c.text.trim().isNotEmpty).toList(), true) >= 2;
+  }
+
+  bool hasDuplicatePlayerNames() {
+    return list_utils.containsDuplicates(playerNameTextEditingControllers.map((c) => c.text.trim()).toList());
+  }
+
+  bool hasGameName() => gameNameTextEditingController.text.isNotEmpty;
+
+  void acceptInputs() {
+    var playerNames = <String>[];
+    var playerColours = <String>[];
+    for (var i = 0; i < 6; i++) {
+      if (playerNameTextEditingControllers[i].text.isNotEmpty) {
+        playerNames.add(playerNameTextEditingControllers[i].text);
+        playerColours.add(colours[i]);
+      }
+    }
+    var newGame = Game(
+      name: gameNameTextEditingController.text,
+      playerNames: playerNames,
+      playerColours: playerColours,
+    );
+    Provider.of<GamesManager>(context, listen: false).addNewGame(newGame);
+    Navigator.of(context).pop();
+  }
+
+  void reportErroneousInputs() {
+    String message;
+    if (!hasGameName()) {
+      message = 'Enter a game name.';
+    } else if (!hasEnoughPlayers()) {
+      message = 'Can\'t play with less than two players.';
+    } else if (hasDuplicatePlayerNames()) {
+      message = 'Can\'t have duplicate player names.';
+    }
+    scaffoldkey.currentState.showSnackBar(SnackBar(
+      content: Text(message),
+    ));
+  }
+
+  void acceptSequence() {
+    trimTextEditingControllers();
+    if (hasGameName() && hasEnoughPlayers() && !hasDuplicatePlayerNames()) {
+      acceptInputs();
+    } else {
+      reportErroneousInputs();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    var fieldDecoration = InputDecoration(
+      hintText: 'HINT TEXT',
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+    );
+
+    var gameNameFieldDecoration = fieldDecoration.copyWith(
+      hintText: 'Game name',
+      hintStyle: TextStyle(fontSize: 20),
+    );
+
+    var playerNameFieldDecoration = fieldDecoration.copyWith(
+      hintText: 'Player name',
+      hintStyle: TextStyle(fontSize: 16),
+    );
+
     return Scaffold(
+      key: scaffoldkey,
       appBar: AppBar(
         title: Text('New Game'),
+        actions: [
+          FlatButton.icon(
+            textColor: Colors.white,
+            onPressed: acceptSequence,
+            icon: Icon(Icons.check),
+            label: Text('Done'),
+          ),
+        ],
       ),
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: NewGameForm(),
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Form(
+          key: formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              // crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 10),
+                TextFormField(
+                  controller: gameNameTextEditingController,
+                  textCapitalization: TextCapitalization.words,
+                  decoration: gameNameFieldDecoration,
+                ),
+                SizedBox(height: 10),
+                ...List.generate(6, (index) {
+                  return Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircleAvatar(
+                          // if no player yet, dull the CircleAvatar
+                          backgroundColor: playerNameTextEditingControllers[index].text.isEmpty
+                              ? colour_utils.fromTextDull(colours[index])
+                              : colour_utils.fromText(colours[index]),
+                        ),
+                        SizedBox(width: 20),
+                        Expanded(
+                          child: TextFormField(
+                            decoration: playerNameFieldDecoration,
+                            textCapitalization: TextCapitalization.words,
+                            controller: playerNameTextEditingControllers[index],
+                            onChanged: (newText) {
+                              // update state to make CircleAvatars "light up"
+                              setState(() {});
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+                // SizedBox(height: 20),
+                // FlatButton(
+                //   child: Text('ACCEPT'),
+                //   onPressed: acceptSequence,
+                // ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
